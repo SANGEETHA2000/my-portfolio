26978956828948e7db9a234b9c108039fc50599b
SANGEETHA2000 <SANGEETHA2000@users.noreply.github.com>
2025-02-20 18:18:28 +0000

    deploy: 511af508024bf9610742fa71e5319b532179f8dd

---
+++ _next/static/chunks/app/layout-8b3b0210c52037fb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{572:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,4475,23)),Promise.resolve().then(l.t.bind(l,5786,23))},5786:()=>{},4475:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_ee5f9b"}}},e=>{var s=s=>e(e.s=s);e.O(0,[44,441,517,358],()=>s(572)),_N_E=e.O()}]);
--- out/_next/static/chunks/app/layout-e1b5cd5b61be6a2d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6710:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,8561,23)),Promise.resolve().then(l.t.bind(l,5786,23))},5786:()=>{},8561:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_ee5f9b"}}},e=>{var s=s=>e(e.s=s);e.O(0,[714,441,517,358],()=>s(6710)),_N_E=e.O()}]);
